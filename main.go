49a6c96e3abb609e77ef0c302c7798d7a1530990
Cody Ernesti <cernesti@nnnc.org>
2025-07-22 17:01:40 -0500

    add session and nameid details

--- main.go
+++ main.go
@@ -12,50 +12,127 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/crewjam/saml"
 	"github.com/crewjam/saml/samlsp"
 )
 
 var tmpl = template.Must(template.New("attributes").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-	<title>{{.Name}}</title>
-	<style>
-		body { font-family: Arial, sans-serif; margin: 2rem; }
-		table { border-collapse: collapse; width: 50%; }
-		th, td { border: 1px solid #ccc; padding: 0.5rem; text-align: left; }
-		th { background-color: #f0f0f0; }
-	</style>
-</head>
-<body>
-	<h1>{{.Name}}</h1>
-	<table>
-		<tr>
-			<th>Attribute</th>
-			<th>Values</th>
-		</tr>
-		{{range $index, $attr := .Attributes}}
-		<tr>
-			<td>{{$attr.Name}}</td>
-			<td>{{$attr.Values}}</td>
-		</tr>
-		{{end}}
-	</table>
-	<br>
-	<div class="center">
-		<a class="pure-button pure-button-red" href="/logout">Logout</a>
-	</div>
-</body>
-</html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>{{.Name}}</title>
+		<style>
+			body { font-family: Arial, sans-serif; margin: 2rem; }
+			table { border-collapse: collapse; width: 50%; }
+			th, td { border: 1px solid #ccc; padding: 0.5rem; text-align: left; }
+			th { background-color: #f0f0f0; }
+		</style>
+	</head>
+	<body>
+		<h1>{{.Name}}</h1>
+		<p>Welcome! This page displays some details about your Single Sign On connection.</p>
+		<h2>Session Details</h2>
+		<table>
+			<tr>
+				<th>Claim</th>
+				<th>Value</th>
+			</tr>
+			<tr>
+				<td>Subject (NameID)</td>
+				<td>{{.JWT.Subject}}</td>
+			</tr>
+			<tr>
+				<td>Issued At</td>
+				<td>{{.JWT.IssuedAt}}</td>
+			</tr>
+			<tr>
+				<td>Expires At</td>
+				<td>{{.JWT.ExpiresAt}}</td>
+			</tr>
+		</table>
+		<h2>NameID Details</h2>
+		<table>
+			<tr>
+				<th>Format</th>
+				<th>Value</th>
+			</tr>
+			<tr>
+				<td>{{.NameID.Format}}</td>
+				<td>{{.NameID.Value}}</td>
+			</tr>
+		</table>
+		<h2>Attributes</h2>
+		<table>
+			<tr>
+				<th>Attribute</th>
+				<th>Values</th>
+			</tr>
+			{{range $index, $attr := .Attributes}}
+			<tr>
+				<td>{{$attr.Name}}</td>
+				<td>{{$attr.Values}}</td>
+			</tr>
+			{{end}}
+		</table>
+		<br>
+		<div class="center">
+			<a class="pure-button pure-button-red" href="/logout">Logout</a>
+		</div>
+	</body>
+	</html>
 `))
+
+// middleware to override JWTSessionCodec 'new' method with a custom one that adds the nameid and nameidformat as attributes
+// its hacky, but we don't have access to the assertion directly from the session code
+type JWTSessionCodec struct {
+	Inner samlsp.JWTSessionCodec
+}
+
+var _ samlsp.SessionCodec = JWTSessionCodec{}
+
+func (c JWTSessionCodec) New(assertion *saml.Assertion) (samlsp.Session, error) {
+	Session, err := c.Inner.New(assertion)
+	if err != nil {
+		return Session, err
+	}
+	// cast to JWTSessionClaims
+	jwtSession, _ := Session.(samlsp.JWTSessionClaims)
+
+	// Add NameID and NameIDFormat as attributes
+	if sub := assertion.Subject; sub != nil {
+		if nameID := sub.NameID; nameID != nil {
+			jwtSession.Attributes["NameID"] = []string{nameID.Value}
+			if nameIDFormat := nameID.Format; nameIDFormat != "" {
+				jwtSession.Attributes["NameIDFormat"] = []string{nameIDFormat}
+			}
+		}
+	}
+
+	return jwtSession, nil
+}
+
+func (c JWTSessionCodec) Encode(s samlsp.Session) (string, error) {
+	return c.Inner.Encode(s)
+}
+
+func (c JWTSessionCodec) Decode(signed string) (samlsp.Session, error) {
+	return c.Inner.Decode(signed)
+}
 
 type attribute struct {
 	Name   string
 	Values string
 }
 
+type nameID struct {
+	Value  string
+	Format string
+}
+
 type templateData struct {
 	Name       string
+	JWT        samlsp.JWTSessionClaims `json:"-"`
+	NameID     nameID
 	Attributes []attribute
 }
 
@@ -116,6 +193,15 @@ func main() {
 		log.Fatalf("Error creating SAML SP: %v", err)
 	}
 
+	// override the default session provider to use our custom JWTSessionCodec
+	sessionProvider := samlsp.DefaultSessionProvider(opts)
+	codec := samlsp.DefaultSessionCodec(opts)
+	sessionProvider.Codec = JWTSessionCodec{
+		Inner: codec,
+	}
+
+	sp.Session = sessionProvider
+
 	// SAML ACS
 	http.Handle("/saml/", sp)
 
@@ -133,16 +219,38 @@ func main() {
 			return
 		}
 
+		// get the jwt information
+		jwtAttributes := session.(samlsp.JWTSessionClaims)
+
+		attributes := customSession.GetAttributes()
+
+		// if nameid and nameidformat are in attributes, put them in the a nameid struct to pass to the template
+		nameidValue := ""
+		nameidFormat := ""
+		if nameIDValues, ok := attributes["NameID"]; ok && len(nameIDValues) > 0 {
+			nameidValue = nameIDValues[0]
+			delete(attributes, "NameID")
+			if nameIDFormatValues, ok := attributes["NameIDFormat"]; ok && len(nameIDFormatValues) > 0 {
+				nameidFormat = nameIDFormatValues[0]
+				delete(attributes, "NameIDFormat")
+			}
+		}
+
+		nameid := nameID{
+			Value:  nameidValue,
+			Format: nameidFormat,
+		}
+
 		var rows []attribute
-		keys := make([]string, 0, len(customSession.GetAttributes()))
-		for k := range customSession.GetAttributes() {
+		keys := make([]string, 0, len(attributes))
+		for k := range attributes {
 			keys = append(keys, k)
 		}
 		sort.Strings(keys)
 		for _, k := range keys {
 			rows = append(rows, attribute{
 				Name:   k,
-				Values: strings.Join(customSession.GetAttributes()[k], ", "),
+				Values: strings.Join(attributes[k], ", "),
 			})
 		}
 
@@ -152,6 +260,8 @@ func main() {
 
 		var tdata = templateData{
 			Name:       appName,
+			JWT:        jwtAttributes,
+			NameID:     nameid,
 			Attributes: rows,
 		}
 
